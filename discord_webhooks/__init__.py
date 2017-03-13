82c98eacbf6efce3ff10631c9dbe8f61031a450a
AraHaan <seandhunt_7@yahoo.com>
2017-03-12 21:11:18 -0500

    Added initial version of discord.webhooks.

---
+++ discord_webhooks/__init__.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+"""
+discord.webhooks
+~~~~~~~~~~~~~~~~~~~
+
+Webhooks Extension to discord.py
+
+:copyright: (c) 2017 AraHaan
+:license: MIT, see LICENSE for more details.
+
+"""
+from .webhooks import *
+
+__title__ = 'discord.webhooks'
+__author__ = 'Decorater'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Decorater'
+__version__ = '0.0.1'
+__build__ = 0x000001
+
+__all__ = webhooks.__all__
