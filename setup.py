7f85d1a4b4589f937dfb38cad1ecd9ac8115b8ea
AraHaan <seandhunt_7@yahoo.com>
2017-03-12 22:37:00 -0500

    Changes to some things.

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+requirements = []
+
+version = '0.0.1'
+
+if not version:
+    raise RuntimeError('version is not set')
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(name='discord.webhooks',
+      author='Decorater',
+      author_email='seandhunt_7@yahoo.com',
+      url='https://github.com/AraHaan/discord.webhooks',
+      bugtrack_url='https://github.com/AraHaan/discord.webhooks/issues',
+      version=version,
+      packages=['discord_webhooks'],
+      license='MIT',
+      description='TinyURL for Python 3.x',
+      long_description=readme,
+      maintainer_email='seandhunt_7@yahoo.com',
+      download_url='https://github.com/AraHaan/discord.webhooks',
+      include_package_data=True,
+      install_requires=requirements,
+      platforms='Any',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+      ]
+)
