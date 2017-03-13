1f3c1dea244a2fefb663e422370d44bd55c0a02b
AraHaan <seandhunt_7@yahoo.com>
2017-03-13 00:17:13 -0500

    Delete upload.bat

--- upload.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"%SystemDrive%\Python360\python.exe" setup.py sdist
-"%SystemDrive%\Python360\python.exe" setup.py bdist_wheel
-"%SystemDrive%\Python360\python.exe" -m twine upload "dist/*"
-pause
